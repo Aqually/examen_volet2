1520388098f2478f7f6611c9a9879da619b82fb2
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-21 14:29:23 -0400

    #1 affichage du fichier JSON

--- etape.js
+++ etape.js
@@ -4,10 +4,16 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const ObjectID = require('mongodb').ObjectID;
 const app = express();
-app.set('view engine', 'ejs'); // générateur de template 
+app.set('view engine', 'ejs'); // générateur de template
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
 
+//acces au fichier à lire sur "/"
+app.get('/',  (req, res) => {
+    res.sendFile(__dirname + "/public/text/collection_provinces.json")
+})
 
+//utiliser le port 8080
+app.listen(8080);
